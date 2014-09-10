af78bb1cfc8524af8cc92b2b3f7e479172a0cc35
buddyavi <avi.sanadhya@gmail.com>
2014-09-09 22:48:27 -0700

    Exception Handling

--- MemcachePopulator/src/com/usc/avi/memcache/daoimpl/MysqlToMemcache.java
+++ MemcachePopulator/src/com/usc/avi/memcache/daoimpl/MysqlToMemcache.java
@@ -14,42 +14,49 @@ import org.slf4j.LoggerFactory;
 import com.usc.avi.memcache.constants.Constants;
 import com.usc.avi.memcache.mysql.MySqlConnection;
 
-
-
-
 /**
  * Class for getting column name to value mapping from mySql db
- *
- */
+ * 
+ */	
 public class MysqlToMemcache {
-	private static Logger logger = LoggerFactory.getLogger(MysqlToMemcache.class);
+	private static Logger logger = LoggerFactory
+			.getLogger(MysqlToMemcache.class);
 	Connection connection = null;
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
 	Statement stmt = null;
-/**
- * Method to get column name to value mapping from mysql DB
- * @return  map 
- */
+
+	/**
+	 * Method to get column name to value mapping from mysql DB
+	 * 
+	 * @return map
+	 */
 	public Map<String, String> getColDatatype() {
-   logger.info("inside getColDatatype() method");
-		connection = MySqlConnection.getConnection();
+		logger.info("inside getColDatatype() method");
+
 		Map<String, String> dataMap = new HashMap<String, String>();
 		try {
+			connection = MySqlConnection.getConnection();
 			stmt = connection.createStatement();
 			rs = stmt.executeQuery(Constants.QUERY_TO_GET_MAPPING);
 			while (rs.next()) {
-				
-				 String column1 = rs.getString("NAME").trim();
-				    String column2 = rs.getString("STUDENT_ID").trim();
-				    dataMap.put(column1, column2);			
+
+				String column1 = rs.getString("NAME").trim();
+				String column2 = rs.getString("STUDENT_ID").trim();
+				dataMap.put(column1, column2);
 			}
 
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			logger.error("SQLException:" + e);
+			System.exit(1);
+
+		} catch (Exception e) {
+			logger.error("Exception:" + e);
+			System.exit(1);
+
 		}
-	logger.info("Column name to value map size: "+dataMap.size());	
+		logger.info("Column name to value map size: " + dataMap.size());
 		return dataMap;
 
 	}
